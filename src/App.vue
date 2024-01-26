955e74c36b144dcd76d8cb16818b096b1ca47e51
sunqi <sunqi@leador.com.cn>
2024-01-26 15:28:39 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,41 @@
 <template>
-  <div
-    :id="id"
-    ref="cesiumContainer"
-    :style="{height,width}"
-  />
+  <div :id="id" ref="cesiumContainer" :style="{ height, width }"></div>
 </template>
 
-<script>
+<script setup>
+import { onMounted, ref } from 'vue'
 import 'cesium/Build/Cesium/Widgets/widgets.css'
 import { Viewer } from 'cesium'
 
-export default {
-  name: 'CesiumMap',
-  props: {
-    id: {
-      type: String,
-      default: (() => Math.random().toString(36).substr(2))()
-    },
-    width: {
-      type: String,
-      default: '100%'
-    },
-    height: {
-      type: String,
-      default: '100%'
-    }
-  },
-  data() {
-    return {
-      viewer: null
-    }
-  },
-  mounted() {
-    this.initCesium()
-  },
-  methods: {
-    initCesium () {
-      const el = this.$refs.cesiumContainer
+const cesiumContainer = ref()
 
-      this.viewer = new Viewer(el, {
-        selectionIndicator: false,
-        infoBox: false,
-        contextOptions: {
-          // 硬件反走样，默认值为 1
-          msaaLevel: 8,
-          requestWebgl2: true
-        },
-        animation: false,
-        timeline: false, // 底部时间线
-        fullscreenButton: false, // 全屏
-        vrButton: false, // VR
-        sceneModePicker: false, // 选择视角的模式（球体、平铺、斜视平铺）
-        baseLayerPicker: false, // 图层选择器（地形影像服务）
-        navigationHelpButton: false, // 导航帮助(手势，鼠标)
-        geocoder: false, // 位置查找工具
-        homeButton: false // 视角返回初始位置
-      })
-    }
-  }
+const initCesium = () => {
+  const el = cesiumContainer.value
+
+  const viewer = new Viewer(el, {
+    selectionIndicator: false,
+    infoBox: false,
+    contextOptions: {
+      // 硬件反走样，默认值为 1
+      msaaLevel: 8,
+      requestWebgl2: true
+    },
+    animation: false,
+    timeline: false, // 底部时间线
+    fullscreenButton: false, // 全屏
+    vrButton: false, // VR
+    sceneModePicker: false, // 选择视角的模式（球体、平铺、斜视平铺）
+    baseLayerPicker: false, // 图层选择器（地形影像服务）
+    navigationHelpButton: false, // 导航帮助(手势，鼠标)
+    geocoder: false, // 位置查找工具
+    homeButton: false // 视角返回初始位置
+  })
+
+  console.log(viewer)
 }
+
+onMounted(() => {
+  initCesium()
+})
+
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
 import { createApp } from 'vue'
+import {createPinia} from 'pinia'
+import ElementPlus from 'element-plus'
+import locale from "element-plus/dist/locale/zh-cn.mjs"
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import 'element-plus/dist/index.css'
+
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const pinia=createPinia()
+
+pinia.use(piniaPluginPersistedstate)
+
+const app=createApp(App)
+
+app.use(ElementPlus,{locale})
+app.use(pinia)
+
+//对话框禁止点击其余区域关闭
+app._context.components.ElDialog.props.closeOnClickModal.default = false
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
+app.mount('#app')
